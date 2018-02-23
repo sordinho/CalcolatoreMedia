1559c674451f392c3bcb396c1ecba6966f37db10
Fabio Somaglia <fabio.somaglia@edu.unito.it>
2018-02-23 13:43:26 +0100

    Eliminata classe Studente e aggiunta in CalcoloMedia

--- src/CalcolatoreMedia.java
+++ src/CalcolatoreMedia.java
@@ -1,21 +1,79 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 /**
- * Title: Main class per il calcolo della media
+ * Title: Main class per il calcolo della media e gestisce gli esami e le statistiche di uno studente
  * Created by Davide Sordi on 16/02/2018 at 15.18.
  */
 
 public class CalcolatoreMedia {
 
+	private List<String> lettura = Utility.readLines("esami.txt");
+	private Map<Integer, Esame> esami = Utility.elaboraEsami(lettura);
+
+	public void stampaLibretto() {
+		System.out.println("ESAME - VOTO - CREDITI");
+		for (Esame e : esami.values()) {
+			if (e.pass()) {
+				System.out.println(e.getNome() + " - " + e.getVoto() + " - " + e.getCrediti());
+			}
+		}
+		return;
+	}
+
+	public void stampaEsamiDaDare() {
+		System.out.println("CODICE - ESAME - CREDITI");
+		for (Esame e : esami.values()) {
+			if (!e.pass())
+				System.out.println(e.geCodice() + " - " + e.getNome() + " - " + e.getCrediti());
+		}
+		return;
+	}
+
+	public void calcolaMedia() {
+		double media = 0;
+		double mediap=0;
+		int crediti = 0;
+		int creditip=0;
+
+		for (Esame e : esami.values()) {
+			if (e.pass()) {
+				media += e.getCrediti() * e.getVoto();
+				crediti += e.getCrediti();
+				mediap += e.getCrediti() * e.getVoto();
+				creditip += e.getCrediti();
+			}
+			if(e.previsto()) {
+				mediap += e.getCrediti() * e.getVoto();
+				creditip+=e.getCrediti();
+			}
+		}
+		media = media / crediti;
+		mediap=mediap/creditip;
+		System.out.println("Media di: " + String.format("%.2f",media) + " calcolata su " + crediti + " crediti");
+		System.out.println("Media con previsioni di: " + String.format("%.2f",mediap) + " calcolata su " + creditip + " crediti");
+		return;
+	}
+
+	public void previsione() {
+		int code;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Inserisci il codice di un esame non dato");
+		code = in.nextInt();
+		if(esami.get(code)==null){
+			System.out.println("Codice inserito non valido");
+			return;
+		}
+		Esame exam=esami.get(code);
+		System.out.println("Inserisci il voto che pensi di prendere");
+		int voto=in.nextInt();
+		exam.setVoto(voto);
+	}
+
     public static void main(String[] args) {
 
-        Studente studente = new Studente();
-        //studente.stampaLibretto();
+        CalcolatoreMedia calcolatoreMedia = new CalcolatoreMedia();
 
         Character cmd ='E';
         Scanner in = new Scanner(System.in);
@@ -25,24 +83,24 @@ public class CalcolatoreMedia {
             switch (cmd) {
                 case 'L':
                 case 'l':
-                    studente.stampaLibretto();
+                    calcolatoreMedia.stampaLibretto();
                     break;
                 case 'M':
                 case 'm':
-                    studente.calcolaMedia();
+                    calcolatoreMedia.calcolaMedia();
                     break;
                 case 'N':
                 case 'n':
-                    studente.stampaEsamiDaDare();
+                    calcolatoreMedia.stampaEsamiDaDare();
                     break;
                 case 'P':
                 case 'p':
-                    studente.stampaEsamiDaDare();
-                    studente.previsione();
+                    calcolatoreMedia.stampaEsamiDaDare();
+                    calcolatoreMedia.previsione();
                     break;
                 case 'D':
                 case 'd':
-                    //studente.deletePrevisione();
+                    //calcolatoreMedia.deletePrevisione();
                     break;
                 case 'E':
                 case 'e':
